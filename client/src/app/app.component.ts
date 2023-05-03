b75e3d00fcb08319e1fd03a97a6729c61a661684
rlatigano <rlatigano@gmail.com>
2023-05-03 11:38:03 -0300

    End of Section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Product } from './models/product';
-import { Pagination } from './models/pagination';
+/*
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
+*/
 
 @Component({
   selector: 'app-root',
@@ -10,17 +12,18 @@ import { Pagination } from './models/pagination';
 })
 export class AppComponent  implements OnInit{
   title = 'Ecommerce';
-  products: Product[] = [];
+ // products: Product[] = [];
 
-  constructor(private http: HttpClient){}
+  //constructor(private http: HttpClient){}
+  constructor(){}
   ngOnInit(): void {
-    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
+  /*  this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
       next: (response) => this.products = response.data, // what to do next
       error: error => console.log(error), // what to do if there is an error
       complete: () => {
         console.log('request completed');
         console.log('extra statement');
       }
-    })
+    })*/
   }
 }
---
+++ client/src/app/shop/shop.component.ts
@@ -0,0 +1,104 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Product } from '../shared/models/product';
+import { ShopService } from './shop.service';
+import { Brand } from '../shared/models/brand';
+import { Type } from '../shared/models/type';
+import { ShopParams } from '../shared/models/shopParams';
+
+@Component({
+  selector: 'app-shop',
+  templateUrl: './shop.component.html',
+  styleUrls: ['./shop.component.scss']
+})
+export class ShopComponent implements OnInit {
+  @ViewChild('search') searchTerm?: ElementRef; // aqui hace referencia al #search en el HTML
+
+  products: Product [] =[];
+  brands: Brand[] = [];
+  types: Type [] = [];
+ /* brandIdSelected = 0;
+  typeIdSelected = 0;
+  sortSelected ='name';
+  */
+ shopParams = new ShopParams();
+
+  sortOptions =[
+    {name:'Alphabetical',value:'name'}, // value es lo que toma el api
+    {name:'Price: Low to high',value:'priceAsc'},  // value es lo que toma el api
+    {name:'Price High to low',value:'priceDesc'}, // value es lo que toma el api
+  ];
+
+  totalCount = 0;
+  
+  constructor(private shopService: ShopService){}
+  ngOnInit(): void {
+   this.getProducts();
+   this.getBrands();
+   this.getTypes();
+   
+  }
+
+  getProducts(){
+    this.shopService.getProducts(this.shopParams).subscribe({
+      next: response => {
+        this.products = response.data;
+        this.shopParams.pageNumber = response.pageIndex;
+        this.shopParams.pageSize = response.pageSize;
+        this.totalCount = response.count;
+        
+      },
+      error: error => console.log(error)
+    })
+
+  }
+  getBrands(){
+    this.shopService.getBrands().subscribe({
+      next: response => this.brands = [{id:0,name:'All'},...response] , //response,
+      error: error => console.log(error)
+    })
+
+  }
+  getTypes(){
+    this.shopService.getTypes().subscribe({
+      next: response => this.types =  [{id:0,name:'All'},...response],
+      error: error => console.log(error)
+    })
+
+  }
+  onBrandSelected(brandId: number){
+    this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+  onTypeSelected(typeId:number){
+    this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+  onSortSelected(event: any ){
+    this.shopParams.sort = event.target.value;
+    this.getProducts();
+  }
+  onPageChange(event: any){
+    if(this.shopParams.pageNumber !== event)
+    {
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
+  }
+
+  onSearch(){
+
+    this.shopParams.search = this.searchTerm?.nativeElement.value;
+    this.shopParams.pageNumber = 1;
+    this.getProducts();
+  }
+  onReset() {
+
+    if(this.searchTerm) this.searchTerm.nativeElement.value ='';
+    this.shopParams = new ShopParams(); // resetea todos los params
+    this.getProducts();
+  }
+
+
+}
