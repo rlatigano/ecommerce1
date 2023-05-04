5bfb54824561ed48eb0c0abb96d3ddbf209ed436
rlatigano <rlatigano@gmail.com>
2023-05-04 12:33:56 -0300

    End of Section 10

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -22,11 +22,12 @@ export class ShopService {
     params = params.append('pageIndex',shopParams.pageNumber);
     params = params.append('pageSize',shopParams.pageSize);
     if(shopParams.search)  params = params.append('search',shopParams.search);
-
-
    //  return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products?pageSize=50');
    return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products',{params});
 
+   }
+   getProduct(id: number){
+    return this.http.get<Product>(this.baseUrl + 'products/'+id);
    }
   getBrands(){
     return this.http.get<Brand[]>(this.baseUrl + 'products/brands');
